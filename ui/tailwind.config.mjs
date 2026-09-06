71d2ecf521767ae3a8773373ff937d1f0a89d491
Marcel Rösler <me@marcel.codes>
2026-09-06 12:25:00 +0200

    e: capture run output for e/pi/run-35

--- ui/tailwind.config.mjs
+++ ui/tailwind.config.mjs
@@ -1,6 +1,7 @@
+import tailwindcssAnimate from 'tailwindcss-animate';
+
 /** @type {import('tailwindcss').Config} */
-/* eslint-disable @typescript-eslint/no-require-imports */
-module.exports = {
+export default {
   darkMode: ['class'],
   content: ['./src/**/*.{ts,tsx}'],
   theme: {
@@ -86,5 +87,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [tailwindcssAnimate],
 };
