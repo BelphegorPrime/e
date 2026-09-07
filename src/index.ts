c7df49b8b676ec9c94d77637672f918d4599c826
Marcel Rösler <me@marcel.codes>
2026-09-07 18:54:16 +0200

    set Version from Git Tag

--- src/index.ts
+++ src/index.ts
@@ -8,26 +8,14 @@ import {
   registerExportCommand,
   registerImportCommand,
 } from './transfer/index.js';
-import { log } from './utils/log.js';
+import { E_VERSION } from './version.js';
 
 const program = new Command();
-
-const getVersion = async (): Promise<string> => {
-  const versionModule = './version.js';
-  let version = '1.0.0';
-  try {
-    const mod = await import(versionModule);
-    version = mod.E_VERSION;
-  } catch {
-    log.info('version.ts/version.js does not exist');
-  }
-  return version;
-};
 
 program
   .name('e')
   .description('CLI tool for my AI Orchestrator')
-  .version(await getVersion());
+  .version(E_VERSION);
 
 registerSpawnCommand(program);
 registerInitCommand(program);
