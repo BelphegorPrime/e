b2f97e719a3010ebc189db3f3a1143e4ea565967
Marcel Rösler <me@marcel.codes>
2026-09-07 18:16:03 +0200

    set Version from Git Tag

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,19 @@ import { Command } from 'commander';
 import { registerSpawnCommand } from './spawn/spawn.js';
 import { registerInitCommand } from './init/index.js';
 import { registerServeCommand } from './serve/serve.js';
-import { registerExportCommand, registerImportCommand } from './transfer/index.js';
+import {
+  registerExportCommand,
+  registerImportCommand,
+} from './transfer/index.js';
+
 const program = new Command();
+
+const version = process.env.E_VERSION ?? '1.0.0';
 
 program
   .name('e')
   .description('CLI tool for my AI Orchestrator')
-  .version('1.0.0');
+  .version(version);
 
 registerSpawnCommand(program);
 registerInitCommand(program);
