0f47c0e7628c00bd11a3aa01f0f4821a0a4372e4
Marcel <BelphegorPrime@users.noreply.github.com>
2026-09-10 12:03:32 +0200

    Delete scripts/generate-completion-hint.mjs

--- scripts/generate-completion-hint.mjs
+++
@@ -1,5 +0,0 @@
-console.log('To enable e completion, add this to your shell config (~/.bashrc or ~/.zshrc):');
-console.log('');
-console.log('  # e completion');
-console.log('  source <(e completion bash)');
-console.log('');
